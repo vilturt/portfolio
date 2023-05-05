0750dab40ff53d85454b466424eaa97655a9d6d1
vilturt <viljamiturtiainen@gmail.com>
2023-05-05 14:22:34 +0300

    Add files via upload

---
+++ js/code.js
@@ -0,0 +1,31 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  function NaytaOsaaminen() {    
+    document.getElementById("contentUpdate").innerHTML = "<h2>Osaaminen</h2> <p>Minulla on vahvaa teknistä ja tietoteknistä osaamista. Näen ympärilläni tällä hetkellä myös mahdollisuuksia kehittyä. Luonteeltani olen luova, rauhallinen ja järkevä. Työelämässä olen ollut joitakin vuosia. Tykkään tehdä töitä itsenäisesti, mutta työskentelen myös luontevasti ryhmässä tai pareittain erilaisten ihmisten kanssa. Olen päässyt koulutukseni jälkeen harjaantumaan ihmisten kohtaamisessa, neuvomisessa, oepttamisessa ja tukemisessas. <br><br> Tekniikoita, joissa minulla on osaamista ovat: HTML, CSS, JavaScript, PHP, MySQL, C/C++-ohjelmointi, Python. Olen hyvä viestimään kirjallisesti. Tykkään kirjoittaa ja se on myös harrastukseni. <br><br> Minulla on hyvät valmiudet oppia uusia asioita. Olen päässyt kehittymään ja kehittämään omia vahvuuksia sekä erilaisia opiskelutekniikoita. Kaikesta ei voi tietää tai oppia kaikkea kerralla, vaan se vaatii paljon aikaa. Uskon, että ihminen tai yksilö oppii koko elämänsä ajan uutta. <br><br> Ohjelmoinnin ja teoreettisen tietoteknisen osaamisen lisäksi minulta löytyy paljon käytännön kokemusta ja osaamista. Linux on Windowsin lisäksi myös jossain määrin tuttua. Ymmärrän tietokoneen toimintaa myös käytännön tasolla.</p> <br><br>";
+  }
+
+  function NaytaTyokokemus() {
+    document.getElementById("contentUpdate").innerHTML = "<h2>Työkokemus</h2> Sähköpostiasiantuntija (Aalto-yliopisto) 2021-> <br> <p>Toimin Aalto-yliopiston IT-tuessa ratkoen käyttäjien ongelmia liittyen sähköpostin käyttöön. Tekniikoita: Outlook, Exchange, PowerShell, Mailman-postituslistat, Thunderbird. </p><br>Sovellusneuvoja (Vantaan Pilvitoimisto) 2020-2021 <p>Vantaan kaupungin työntekijöiden opastus ja perehdytys puhelinten ja tietokoneiden sekä ohjelmien käyttöön ja käyttöönottoon liittyen. M365-sovellusten käytön opastaminen. </p> <br> Asiakaspalvelija (Game House) 2018 <br> <p>Toimin pelikaupasssa palvellen asiakkaita puhelimitse, sähköpostitse ja kasvotusten. Verkkokaupan ylläpitoa WordPress ja WooCommerce työvälineillä. Blogin kirjoittamista. Tilausten postitus. Toimin tehtävässä 6 kuukautta työkokeilussa. </p> <br><br>";
+  }
+
+
+  function NaytaKoulutus() {
+    document.getElementById("contentUpdate").innerHTML = "<h2>Koulutus</h2> Insinööri - Tietotekniikka/Ohjelmistotekniikka (2010-2016) <br> <p>Suoritin Metropoliassa Insinöörin AMK-tutkinnon. Aloitin opiskelun Tietotekniikassa jossa syvennyin Ohjelmistotekniikkaan. Opin ohjelmoinnin perusteet usealla eri ohjelmointikielellä (Java, PHP, Python, C, Ruby), versionhallintaa (Git, Subversion), olio-ohjelmointia, käyttäjäkeskeistä sekä käyttöliittymien suunnittelua, ohjelmisto- ja käyttäjätestausta, työskentelyä ryhmissä/pareittain, viestintää ruotsiksi, suomeksi ja englanniksi. Raporttien ja esitysten laatiminen ja pitäminen. Projektityöskentely (Scrum). <br><br> Suoritin kurssin teknologiayrityksen johtamisesta. Tiedonlouhinta, matematiikka, fysiikka, tiedonhallinnan perusteet. Pääsin työskentelemään Innovaatioprojektissa pienessä ryhmässä, jossa toteutimme web-sovelluksen PHP Symfony -sovelluskehystä käyttäen. Lopputyönä tein web-sovelluksen, jossa oli muutama taulua sisälläänpitävä MySQL-tietokanta. Käytin siinä myös tekniikoita (HTML, CSS, PHP, JavaScript). </p> <br> Elektroniikka-asentaja (2008-2010) <br> <p>Elektronisten peruskomponenttien tunnistaminen, erilaiset mittaukset analogisilla ja digitaalisilla yleismittareilla, harjoitustöiden raportointi ja dokumentointi, juottaminen, sähköopin ja fysiikan perusteet. Opetukseen kuului 2 x 3kk työssäoppimisjaksoja, jotka suoritin InfoCarella kannettavia tietokoneita huoltaen.</p> <br><br>";
+  }
+  
+  function NaytaHar() {
+    document.getElementById("contentUpdate").innerHTML = "<h2>Harrastukset</h2> <p>Blogin kirjoittaminen, videopelien pelaaminen (Xbox Series X, PS4, PC, myös retropelit), musiikin tekeminen (sampleri/tietokone), sanoitusten kirjoittaminen, lukeminen (mm. tietokirjat, elämänkerrat ja sci-fi), käveleminen. <br><br> Ohjelmoinnin harjoittelu, pelisuunnittelu (lyhyiden prototyyppien tekeminen ja ohjelmoinnin harjoittelu, JavaScript, PHP, Java, C), kotisivujen tekeminen (HTML, CSS, JavaScript, WordPress, PHP).</p>";
+  }
+
+  function NaytaYht() {
+    document.getElementById("contentUpdate").innerHTML = "<h2>Yhteystiedot</h2> E-mail: viljamiturtiainen@gmail.com <br> Blog: <a href='https://www.intensivegamingdiary.com'>Intensive Gaming Diary</a> <br> LinkedIn: <a href='https://www.linkedin.com/in/viljami-turtiainen-26aa93123'>Link to Profile</a><br><br>Musiikkia: <br> <a href='https://www.mikseri.net/vilik'>Mikseri.net</a><br> <a href='https://www.soundcloud.com/vilikoo'>SoundCloud</a> <br><br>";
+  }
+
+
